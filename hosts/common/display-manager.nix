401731a9ac1b6af378a9f572c9f0c8fcda90c9cf
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2025-01-26 16:22:44 +0100

    Use SDDM

--- hosts/common/display-manager.nix
+++ hosts/common/display-manager.nix
@@ -1,17 +1,8 @@
 {pkgs, ...}:
 {
-  services.greetd = {
+  services.display-manager.sddm {
     enable = true;
-
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.tuigreet}/bin/tuigreet --time --time-format '%H:%M | %a • %h | %d/%m/%Y' --remember --remember-session --user-menu --window-padding 1 --cmd Hyprland";
-        user = "greeter";
-      };
-    };
+    wayland.enable = true;
+    autoNumlock = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    greetd.tuigreet
-  ];
 }
