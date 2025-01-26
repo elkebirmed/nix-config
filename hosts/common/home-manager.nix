43698cdc130359b9363529ef31106cfdcd977750
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2025-01-26 14:25:29 +0100

    .....

--- hosts/common/home-manager.nix
+++ hosts/common/home-manager.nix
@@ -2,7 +2,7 @@
 {
   imports = [ inputs.home-manager.nixosModules.home-manager ];
 
-  home-manager = {
+  inputs.home-manager = {
     useUserPackages = true;
     useGlobalPkgs = true;
     extraSpecialArgs = {inherit inputs outputs;};
