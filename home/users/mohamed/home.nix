dde3664bec64a103cfcd9a00099c7df5c75e2f9b
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2025-01-26 14:22:12 +0100

    ...

--- home/users/mohamed/home.nix
+++ home/users/mohamed/home.nix
@@ -52,10 +52,4 @@
   };
 
   programs.vscode.enable = true;
-
-  # Nicely reload system units when changing configs
-  systemd.user.startServices = "sd-switch";
-
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "24.11";
 }
