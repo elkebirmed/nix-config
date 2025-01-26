0ef158184765f20a2a2ca07c7cd246f9ecf673d1
Mohamed Elkebir <mohamedelkebir85@gmail.com>
2025-01-26 10:20:57 +0100

    Fix localization

--- hosts/crazy/configuration.nix
+++ hosts/crazy/configuration.nix
@@ -84,15 +84,15 @@
   i18n.defaultLocale = "en_US.UTF-8";
 
   i18n.extraLocaleSettings = {
-    LC_ADDRESS = "fr_DZ.UTF-8";
-    LC_IDENTIFICATION = "fr_DZ.UTF-8";
-    LC_MEASUREMENT = "fr_DZ.UTF-8";
-    LC_MONETARY = "fr_DZ.UTF-8";
-    LC_NAME = "fr_DZ.UTF-8";
-    LC_NUMERIC = "fr_DZ.UTF-8";
-    LC_PAPER = "fr_DZ.UTF-8";
-    LC_TELEPHONE = "fr_DZ.UTF-8";
-    LC_TIME = "fr_DZ.UTF-8";
+    LC_ADDRESS = "fr_FR.UTF-8";
+    LC_IDENTIFICATION = "fr_FR.UTF-8";
+    LC_MEASUREMENT = "fr_FR.UTF-8";
+    LC_MONETARY = "en_US.UTF-8";
+    LC_NAME = "fr_FR.UTF-8";
+    LC_NUMERIC = "en_US.UTF-8";
+    LC_PAPER = "fr_FR.UTF-8";
+    LC_TELEPHONE = "fr_FR.UTF-8";
+    LC_TIME = "fr_FR.UTF-8";
   };
 
   # Hyprland
